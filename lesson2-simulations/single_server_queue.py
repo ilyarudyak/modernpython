6a8ab811c69a32576ada0a8105d326b8be602f6a
Ilya Rudyak <ilyarudyak@yahoo.com>
2017-12-20 14:50:53 +0300

    misc changes

--- lesson2-simulations/single_server_queue.py
+++ lesson2-simulations/single_server_queue.py
@@ -1,4 +1,4 @@
-from random import expovariate, gauss
+from random import expovariate, gauss, seed
 from statistics import mean, median, stdev
 
 if __name__ == '__main__':
@@ -12,22 +12,32 @@ if __name__ == '__main__':
     starts = []
     arrival = service_end = 0.0
 
-    ITERATIONS = 20000
+    seed(42)
+    arrival_flag = False
+
+    ITERATIONS = 10  # 20000
 
     for i in range(ITERATIONS):
         if arrival <= service_end:
             num_waiting += 1
             arrival += expovariate(1.0 / average_arrival_interval)
-            arrivals.append(arrival)
+            arrivals.append((f'{arrival:.1f}', num_waiting))
+            arrival_flag = True
         else:
             num_waiting -= 1
             service_start = service_end if num_waiting else arrival
             service_time = gauss(average_service_time, stdev_service_time)
             service_end = service_start + service_time
-            starts.append(service_start)
+            starts.append((f'{service_start:.1f}-{service_time:.1f}-{service_end:.1f}', num_waiting))
+        print(f'it:{i:d} wait:{num_waiting:d}', end='')
+        print(' arrival') if arrival_flag else print()
+        arrival_flag = False
 
-    waits = [start - arrival for arrival, start in zip(arrivals, starts)]
-    print(f'Mean wait: {mean(waits):.1f} Stdev wait: {stdev(waits):.1f}')
-    print(f'Median wait: {median(waits):.1f} Max wait: {max(waits):.1f}')
+    print(arrivals)
+    print(starts)
+
+    # waits = [start - arrival for arrival, start in zip(arrivals, starts)]
+    # print(f'Mean wait: {mean(waits):.1f} Stdev wait: {stdev(waits):.1f}')
+    # print(f'Median wait: {median(waits):.1f} Max wait: {max(waits):.1f}')
 
 
